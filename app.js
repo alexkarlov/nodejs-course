fa5585f545a4f8d13799404c3a81744b49977e1f
okarlov <alex.karlov34@gmail.com>
2020-11-12 00:23:56 +0200

    task3: added CRUD for events

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const http = require('http');
-const Logger = require('./logger.js').createLogger()
-const Configs = require('./config.js').Configs
-
-http.createServer((req, res) => {
-    Logger.log('Hello world!');
-    res.end();
-}).listen(Configs.PORT, () => Logger.log(`Listening on port ${Configs.PORT} on ${Configs.ENV} env...`));
+const express = require("express");
+const app = express();
+const path = require("path");
+const handlers = require(path.join(__dirname, "events", "handlers.js"));
+app.use(express.json());
+app.get("/events/:eventId", handlers.getEventById);
+app.get("/events", handlers.getEvents);
+app.post("/events", handlers.createEvent);
+app.put("/events/:eventId", handlers.updateEvent);
+app.delete("/events/:eventId", handlers.deleteEvent);
+app.get("/events-batch", handlers.getEventsBatch);
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
---
+++ events/handlers.js
@@ -0,0 +1,154 @@
+const path = require("path");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const csv = require("csv-parser");
+const fs = require("fs");
+const converter = require("json-2-csv");
+const { EOF } = require("dns");
+
+const filePath = path.join(__dirname, "events.csv");
+const headerFile = [
+  { id: "id", title: "id" },
+  { id: "title", title: "title" },
+  { id: "location", title: "location" },
+  { id: "timestamp", title: "timestamp" },
+];
+
+function getEventById(req, res) {
+  let event = {};
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      // if we found one row no need to check others
+      if (Object.keys(event).length !== 0) {
+        return;
+      }
+      if (data.id == req.params.eventId) {
+        event = data;
+      }
+    })
+    .on("end", () => {
+      if (Object.keys(event).length === 0) {
+        res.status(404).send("event not found");
+      } else {
+        res.send(event);
+      }
+    });
+}
+
+function getEvents(req, res) {
+  let results = [];
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      if (
+        req.query.location === undefined ||
+        req.query.location == data.location
+      ) {
+        results.push(data);
+      }
+    })
+    .on("end", () => {
+      res.send(results);
+    });
+}
+
+function createEvent(req, res) {
+  // just add at the end of the file
+  fs.open(filePath, "a", 666, function (e, id) {
+    // convert JSON array to CSV string
+    converter.json2csv(
+      req.body,
+      (err, csv) => {
+        if (err) {
+          res.status(500).send();
+          return;
+        }
+        const row = "\n" + csv;
+        fs.write(id, row, null, "utf8", function () {
+          fs.close(id, function () {
+            res.status(200).send();
+          });
+        });
+      },
+      { prependHeader: false }
+    );
+  });
+}
+
+function updateEvent(req, res) {
+  let results = [];
+  let found = false;
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      if (data.id == req.params.eventId) {
+        found = true;
+        results.push(req.body);
+      } else {
+        results.push(data);
+      }
+    })
+    .on("end", () => {
+      if (found === false) {
+        res.status(404).send("event not found");
+        return;
+      }
+      const csvWriter = createCsvWriter({
+        path: filePath,
+        header: headerFile,
+      });
+      csvWriter.writeRecords(results).then(() => {
+        console.log("The CSV file was written successfully");
+        res.status(200).send();
+      });
+    });
+}
+
+function deleteEvent(req, res) {
+  let results = [];
+  let found = false;
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      if (data.id == req.params.eventId) {
+        found = true;
+        return;
+      }
+      results.push(data);
+    })
+    .on("end", () => {
+      if (found === false) {
+        res.status(404).send("event not found");
+        return;
+      }
+      const csvWriter = createCsvWriter({
+        path: filePath,
+        header: headerFile,
+      });
+      csvWriter.writeRecords(results).then(() => {
+        console.log("The CSV file was written successfully");
+        res.status(200).send();
+      });
+    });
+}
+
+function getEventsBatch(req, res) {
+  let results = [];
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      results.push(data);
+    })
+    .on("end", () => {
+      res.send(results);
+    });
+}
+
+module.exports = {
+  getEventById: getEventById,
+  getEvents: getEvents,
+  createEvent: createEvent,
+  updateEvent: updateEvent,
+  deleteEvent: deleteEvent,
+  getEventsBatch: getEventsBatch,
+};
